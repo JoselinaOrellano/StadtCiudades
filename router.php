311f2a5dda9adb65785250f9b81e8c782bcdad8b
JoselinaOrellano <63608101+JoselinaOrellano@users.noreply.github.com>
2020-05-31 12:18:53 -0300

    Creación del router

--- router.php
+++ router.php
@@ -1,1 +1,16 @@
 ?php
+<?php
+    define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+    require_once 'controllers/ciudades.controller.php';
+    
+    $action= $_REQUEST['action'];
+    $urlAction= explode('/',$action);
+
+ 
+    switch ($urlAction[0]){
+        ccacase'inicio': {
+            $ctrl= new ciudadesControlador();
+           $ctrl ->imprimirInicio();
+        } break;
+    }
